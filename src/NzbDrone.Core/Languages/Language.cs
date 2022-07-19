85999ad64922b5b137400f3e584d0119f1a29bd7
dtalens <databio@gmail.com>
2022-07-19 20:06:00 +0200

    Update Language.cs
    Add Catalan into language list

--- src/NzbDrone.Core/Languages/Language.cs
+++ src/NzbDrone.Core/Languages/Language.cs
@@ -105,6 +105,7 @@ namespace NzbDrone.Core.Languages
         public static Language Ukrainian => new Language(32, "Ukrainian");
         public static Language Persian => new Language(33, "Persian");
         public static Language Bengali => new Language(34, "Bengali");
+        public static Language Catalan => new Language(35, "Catalan");
         public static Language Any => new Language(-1, "Any");
         public static Language Original => new Language(-2, "Original");
 
@@ -149,6 +150,7 @@ namespace NzbDrone.Core.Languages
                     Ukrainian,
                     Persian,
                     Bengali,
+                    Catalan,
                     Any,
                     Original
                 };
